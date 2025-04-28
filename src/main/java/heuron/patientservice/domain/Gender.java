cfd30c91c6b4a1172e727f3addb09c533d417b79
won <jwon3991.9191@gmail.com>
2025-04-28 19:21:35 +0900

    feat: patient jpa 엔티티 추가

---
+++ src/main/java/heuron/patientservice/domain/Gender.java
@@ -0,0 +1,6 @@
+package heuron.patientservice.domain;
+
+public enum Gender {
+    MALE,
+    FEMALE
+}
---
+++ src/main/java/heuron/patientservice/domain/model/Patient.java
@@ -0,0 +1,39 @@
+package heuron.patientservice.domain.model;
+
+import heuron.patientservice.domain.Gender;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Table(name = "patient")
+public class Patient {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private Integer age;
+
+    @Enumerated(EnumType.STRING)
+    private Gender gender;
+
+    private boolean disease;
+    private String image_path;
+
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createdAt;
+
+    @LastModifiedDate
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+
+}
