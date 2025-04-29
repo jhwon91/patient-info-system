44f2fe25d94a32cc1fc0c1470f682cc5d3d702a5
won <jwon3991.9191@gmail.com>
2025-04-29 15:44:14 +0900

    feat: jpa 구현체 추가

--- src/main/java/heuron/patientservice/infra/impl/PatientRepositoryImpl.java
+++ src/main/java/heuron/patientservice/infra/impl/PatientRepositoryImpl.java
@@ -1,4 +1,35 @@
 package heuron.patientservice.infra.impl;
 
-public class PatientRepositoryImpl {
+import heuron.patientservice.domain.model.Patient;
+import heuron.patientservice.domain.repository.PatientRepository;
+import heuron.patientservice.infra.jpa.JpaPatientRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+@RequiredArgsConstructor
+public class PatientRepositoryImpl implements PatientRepository {
+    private final JpaPatientRepository jpaPatientRepository;
+
+    @Override
+    public Patient save(Patient patient) {
+        return jpaPatientRepository.save(patient);
+    }
+
+    @Override
+    public Optional<Patient> findByIdAndImageUploadedTrue(Long patientId) {
+        return jpaPatientRepository.findByIdAndImageUploadedTrue(patientId);
+    }
+
+    @Override
+    public Optional<Patient> findById(Long patientId) {
+        return jpaPatientRepository.findById(patientId);
+    }
+
+    @Override
+    public void delete(Patient patient) {
+        jpaPatientRepository.delete(patient);
+    }
 }
